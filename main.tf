694ab533a3ac2e5184aad32de5fb13309be57f4b
Tharini21 <r.tharini@infosys.com>
2025-01-23 20:38:21 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -19,22 +19,6 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-# Create Route Table
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-# Associate Subnet with Route Table
-resource "aws_route_table_association" "rta" {
-  subnet_id      = aws_subnet.subnet.id
-  route_table_id = aws_route_table.rt.id
-}
-
 # Create Security Group with Inbound and Outbound Rules
 resource "aws_security_group" "sg" {
   vpc_id = aws_vpc.main.id
